8446883928d305ff81f0bb358809f9cb75c59a68
Fikir <fikirmuluken@gmail.com>
2021-07-14 21:33:25 +0300

    fix package.json

--- server/src/server.js
+++ server/src/server.js
@@ -2,11 +2,20 @@ const http = require('http');
 
 const app = require('./app');
 
+const { loadPlanetsData } = require('./models/planets.model');
+
 const PORT = process.env.PORT || 8000;
 //defaults to 8000 if not defined on the environmental variable on package.json
 
 const server = http.createServer(app);
 
+async function startServer() {
+    await loadPlanetsData();
 server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+    console.log(`Listening on port: ${PORT} ...`);
 });
+}
+
+startServer();
+
+
