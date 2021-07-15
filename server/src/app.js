077a38eee3a617a2543b8840375540dde4bb9a6e
Fikir <fikirmuluken@gmail.com>
2021-07-15 20:49:28 +0300

    create the launches model and subsequent files

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
-const planetRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
+const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
 
@@ -10,13 +12,17 @@ app.use(cors({
     origin: 'http://localhost:3000',
 }));
 
+app.use(morgan('combined'));
+
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 })
-app.use(planetRouter);
+
+app.use(planetsRouter);
+app.use(launchesRouter);
 
 module.exports = app;
