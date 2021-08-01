1288c98f86d908c9daf20570b9d845c22f0824f2
Fikir <fikirmuluken@gmail.com>
2021-08-01 21:22:55 +0300

    abortLaunch function with mongoose

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -22,8 +22,10 @@ saveLaunch(launch);
 
 // launches.set(launch.flightNumber, launch);
 
-function existsLaunchWithId(launchId) {
-    return launches.has(launchId);
+async function existsLaunchWithId(launchId) {
+    return await launchesDatabase.findOne({
+        flightNumber: launchId,
+    });
 }
 
 async function getLatestFlightNumber() {
@@ -75,11 +77,16 @@ async function scheduleNewLaunch(launch) {
 }
 
 
-function abortLaunchById(launchId) {
-    const aborted = launches.get(launchId);
-    aborted.upcoming = false;
-    aborted.success = false;
-    return aborted;
+async function abortLaunchById(launchId) {
+    const aborted = await launchesDatabase.updateOne({
+        flightNumber: launchId,
+    }, {
+        upcoming: false,
+        success: false,
+    });
+
+    return aborted.ok === 1 && aborted.nModified === 1;
+    
 }
 
 module.exports = {
