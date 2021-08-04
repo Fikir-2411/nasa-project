237c91d0b795dad8c1f7d60a1ec1e57854f55131
Fikir <fikirmuluken@gmail.com>
2021-08-04 14:53:30 +0300

    test code for GET /planets

---
+++ server/src/routes/planets/planets.test.js
@@ -0,0 +1,26 @@
+const request = require('supertest');
+const app = require('../../app');
+const { 
+    mongoConnect,
+    mongoDisconnect,
+ } = require('../../services/mongo');
+
+
+ describe('Planets API', () => {
+     beforeAll(async () =>{
+         await mongoConnect();
+     });
+
+     afterAll(async () => {
+         await mongoDisconnect();
+     });
+
+     describe('Test GET /planets', () => {
+         test('Should return 200 success', async () => {
+             const response = await request(app)
+                .get('/v1/planets')
+                .expect('Content-Type', /json/)
+                .expect(200);
+         });
+     });
+ });
